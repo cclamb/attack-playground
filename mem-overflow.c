65d39307fcf1b02bf552b2cb944d7bd785b796b0
cclamb <cclamb@sandia.gov>
2016-10-10 13:17:50 -0600

    turned off FORTIFY_SOURCE flag to enable stack smashing

---
+++ mem-overflow.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+#define EXIT_OK       1
+
+#define BUFFER_SIZE   10
+static int overflowable(const char* buffer) {
+  char internal_buffer[BUFFER_SIZE];
+  memcpy(internal_buffer, buffer,strlen(buffer));
+  return EXIT_OK;
+}
+
+int main(int argc, char* argv[]) {
+  printf("Staring...\n...calling overflow function.\n");
+  printf("...submitted buffer: %s.\n", argv[1]);
+  int ret = overflowable(argv[1]);
+  return ret;
+}
