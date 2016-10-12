41babc4ccbba1978fa395a4d7f727f04e8181c78
cclamb <cclamb@sandia.gov>
2016-10-12 16:47:33 -0600

    changes to makefile added notes made returnable(.) non-static

--- str-overflow.c
+++ str-overflow.c
@@ -1,9 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define EXIT_OK       1
+#define EXIT_OK       0
+#define EXIT_NO_ARG   1
 
 #define BUFFER_SIZE   10
+
+void returnable(void) {
+  printf("Exiting! You have redirected execution.\n");
+  exit(0);
+}
+
 static int overflowable(const char* buffer) {
   char internal_buffer[BUFFER_SIZE];
   strcpy(internal_buffer, buffer);
@@ -11,6 +19,7 @@ static int overflowable(const char* buffer) {
 }
 
 int main(int argc, char* argv[]) {
+  if (argc < 2) return EXIT_NO_ARG;
   printf("Staring...\n...calling overflow function.\n");
   printf("...submitted buffer: %s.\n", argv[1]);
   int ret = overflowable(argv[1]);
